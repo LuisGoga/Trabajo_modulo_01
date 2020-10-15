ac3af87fd51bb0faba3237258dc9ca9b762c78b4
LuisGoga <66684210+LuisGoga@users.noreply.github.com>
2020-10-15 11:29:47 +0200

    Add files via upload

---
+++ Copia de seguridad_15_10_2020/IconGlobal/iconglobal.js
@@ -0,0 +1,17 @@
+/*
+addEventListener('DOMContentLoaded', ()=>{
+
+    const btn_menu= document.querySelector('.btn_menu')
+
+    if(btn_menu){
+
+        btn_menu.addEventListener('click', ()=>{
+
+            const menu_items = document.querySelector('.meu_items')
+
+             menu_items.classList.toggle('show')
+        })
+    }
+})
+
+/*
